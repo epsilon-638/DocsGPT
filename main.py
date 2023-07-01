96849acf6129394268cc2f7d7808c54e21b7d8ce
Noah Brooks <noahebrooks@protonmail.ch>
2023-07-01 16:01:52 -0400

    feat: add repl command and repl action

--- main.py
+++ main.py
@@ -2,21 +2,41 @@ import argparse
 from langchain.prompts import PromptTemplate
 from agent import agent
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument("url")
-parser.add_argument(
-  "-q",
-  "--question",
-)
-
-args = parser.parse_args()
-
 prompt = """Fetch the response of the documentation url '{url}' and answer a user question about the contents of the page. Question about the response text: {question}"""
 prompt_template = PromptTemplate(
   input_variables=["url", "question"],
   template=prompt,
 )
+class Repl(argparse.Action):
+  def __init__(self, nargs=0, **kw):
+    super().__init__(nargs=nargs, **kw)
+  def __call__(self, parser, namespace, values, option_string=None):
+    print("Enter :q to quit repl\n")
+    while True:
+      url = input("Enter URL: ")
+      if url == ':q': break
+
+      question = input("\nWhat question do you have?: ")
+      if question == ':q': break
+
+      print(agent.run(prompt_template.format(url=url, question=question)))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+  "--start-repl",
+  action=Repl
+)
+parser.add_argument("url", nargs='?')
+parser.add_argument(
+  "-q",
+  "--question",
+  nargs='?'
+)
+
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-  print(agent.run(prompt_template.format(url=args.url, question=args.question)))
+  if args.url and args.question:
+    print(agent.run(prompt_template.format(url=args.url, question=args.question)))
