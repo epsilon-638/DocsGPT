40f2aa5190de3b6554d265c723e7b7e480b07b53
Noah Brooks <noahebrooks@protonmail.ch>
2023-07-01 19:09:18 -0400

    fix: add new param to first step

--- prompt.py
+++ prompt.py
@@ -1,6 +1,6 @@
 from langchain.prompts import PromptTemplate
 
-prompt = """Fetch the response of the documentation url '{url}' and answer a user question about the contents of the page. Question about the response text: {question}"""
+prompt = """First step, fetch the response of the documentation using both the url: '{url}' as well as a user defined question: '{question}'. Second step, answer a user question about the contents of the page. Question about the response text: {question}"""
 prompt_template = PromptTemplate(
   input_variables=["url", "question"],
   template=prompt,
