605da468f7523c51d60fda4835ab1142cbb9dc75
Noah Brooks <noahebrooks@protonmail.ch>
2023-07-01 19:09:08 -0400

    fix: structured tool

--- agent.py
+++ agent.py
@@ -1,5 +1,4 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
 from langchain.experimental.plan_and_execute import (
     PlanAndExecute,
     load_agent_executor,
@@ -11,14 +10,6 @@ from doc_tools import doc_tool
 
 chat = ChatOpenAI(temperature=0)
 llm = OpenAI(temperature=0)
-tools = [
-  Tool(
-    name="Fetch Documentation",
-    func=doc_tool.run,
-    description="Fetch and return response of documentation using a user supplied url",
-  ),
-]
-
 planner = load_chat_planner(llm)
-executor = load_agent_executor(llm, tools, verbose=True)
+executor = load_agent_executor(llm, [doc_tool], verbose=True)
 agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
