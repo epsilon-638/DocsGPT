4b78f4f7c16abf06e245d10eb8f11cda19a2032d
Noah Brooks <noahebrooks@protonmail.ch>
2023-07-01 15:05:29 -0400

    feat: add cli arg parser

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-from langchain.chat_models import ChatOpenAI
+import argparse
 from langchain.prompts import PromptTemplate
-from langchain.experimental.plan_and_execute import (
-    PlanAndExecute,
-    load_agent_executor,
-    load_chat_planner
+from agent import agent
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("url")
+parser.add_argument(
+  "-q",
+  "--question",
 )
-from langchain.agents.tools import Tool
-from langchain.llms import OpenAI
-from doc_tools import doc_tool
 
-chat = ChatOpenAI(temperature=0)
-llm = OpenAI(temperature=0)
-tools = [
-  Tool(
-    name="Fetch Documentation",
-    func=doc_tool.run,
-    description="Fetch and return response of documentation using a user supplied url",
-  ),
-]
-
-planner = load_chat_planner(llm)
-executor = load_agent_executor(llm, tools, verbose=True)
-agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
+args = parser.parse_args()
 
 prompt = """Fetch the response of the documentation url '{url}' and answer a user question about the contents of the page. Question about the response text: {question}"""
-
 prompt_template = PromptTemplate(
   input_variables=["url", "question"],
   template=prompt,
 )
 
-url = "https://sre.google/sre-book/production-environment/"
-question = "What is Borg?"
+if __name__ == "__main__":
+  # url = "https://sre.google/sre-book/production-environment/"
+  # question = "What is Borg?"
 
-print(agent.run(prompt_template.format(url=url, question=question)))
+  print(agent.run(prompt_template.format(url=args.url, question=args.question)))
